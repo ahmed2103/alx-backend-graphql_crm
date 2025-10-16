faae9cc5478e42aa88b80854fa511f0ad1dfa99e
Hasona <bekoahmed20@gmail.com>
2025-10-16 19:39:42 +0300

    .correct name

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,7 +1,7 @@
 import graphene
 
 
-class Query(graphene.ObjectType):
+class Query(CRMQuery, graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
 
 schema = graphene.Schema(query=Query)
